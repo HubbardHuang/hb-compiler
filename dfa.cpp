22c5d06681e7ea699bfbd0ac4abd52ca17369952
HubbardHuang <hubbard_huang@sina.com>
2019-05-14 21:29:46 +0800

    DFA-2
    Done: DFA initialization and running.
    TODO: Test of these code.

--- dfa.cpp
+++ dfa.cpp
@@ -2,6 +2,60 @@
 
 namespace hcc {
 
+int
+DFA::Go(int curr_vertex, char c) {
+    if (graph[curr_vertex].empty()) {
+        return -1;
+    }
+    for (auto road : graph[curr_vertex]) {
+        if (road.weight & code_of[c]) {
+            return road.neighbor;
+        }
+    }
+    return -1;
+}
+
+DFA::~DFA() {}
+
+DFA::DFA(const std::vector<XYZ>& triad, const std::map<char, int>& bit_of) {
+    code_of = bit_of;
+
+    int vertex_count = 0;
+    for (auto t : triad) {
+        if (vertex_count < t.start) {
+            vertex_count = t.start;
+        }
+        if (vertex_count < t.end) {
+            vertex_count = t.end;
+        }
+    }
+    graph.resize(vertex_count + 1);
+    for (auto t : triad) {
+        graph[t.start].push_back(Road(t.end, t.weight));
+    }
+
+    for (int i = 0; i < graph.size(); i++) {
+        if (graph[i].empty()) {
+            end_vertex = i;
+            break;
+        }
+    }
+
+    const int kTrue = 1;
+    const int kFalse = 0;
+    vector<int> be_neighbor(vertex_count, kFalse);
+    for (auto g : graph) {
+        for (auto v : g) {
+            be_neighbor[v.neighbor] = kTrue;
+        }
+    }
+    for (int i = 0; i < be_neighbor.size(); i++) {
+        if (!be_neighbor[i]) {
+            start_vertex = i;
+        }
+    }
+}
+
 bool
 DFA::Judge(const std::string& unit) {
     if (unit.empty()) {
@@ -9,12 +63,16 @@ DFA::Judge(const std::string& unit) {
     }
     int curr_vertex = start_vertex;
     for (auto ch : unit) {
-        if (CanGo(ch)) {
-
+        int next_vertex;
+        if ((next_vertex = Go(curr_vertex, ch)) != -1) {
+            curr_vertex = next_vertex;
+        } else if (curr_vertex == end_vertex) {
+            break;
         } else {
             return false;
         }
     }
+    return true;
 }
 
 } // namespace hcc
--- dfa.h
+++ dfa.h
@@ -7,15 +7,29 @@
 
 namespace hcc {
 
+struct XYZ {
+    int start;
+    int end;
+    int weight;
+};
+
 class DFA {
 private:
+    using Road = struct Road {
+        int neighbor;
+        int weight;
+    };
     int start_vertex;
     int end_vertex;
-    std::vector<std::vector<int>> graph;
-    int DecodeWeight(int);
-    bool CanGo(char ch);
+    std::map<char, int> code_of;
+    std::vector<std::vector<Road>> graph;
+
+    // int DecodeWeight(int);
+    int Go(int, char);
 
 public:
+    ~DFA();
+    DFA(const std::vector<XYZ>&, const std::map<char, int>&);
     bool Judge(const std::string&);
 };
 
